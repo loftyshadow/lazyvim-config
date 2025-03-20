4e310be7a30780a4226f7fe2111a4e12981e6fb8
niemingzhi <1094290505@qq.com>
2025-03-20 10:07:09 +0800

    first commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,39 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- ---------- 插入模式 ---------- ---
+keymap.set("i", "jk", "<ESC>")
+
+-- ---------- 视觉模式 ---------- ---
+-- 单行或多行移动
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- x do not yank
+keymap.set("v", "x", "_x")
+
+-- ---------- 正常模式 ---------- ---
+-- 全选
+keymap.set("n", "vae", "ggVG")
+-- 取消高亮
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+-- 复制到下一行
+keymap.set("n", "<leader>p", "o<ESC>p")
+
+--  保存
+keymap.set({ "n", "i" }, "<C-s>", "<Esc>:w<cr>")
+
+keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
+-- 选择窗口
+keymap.set({ "t", "i" }, "<A-h>", "<C-\\><C-N><C-w>h", { noremap = true })
+keymap.set({ "t", "i" }, "<A-j>", "<C-\\><C-N><C-w>j", { noremap = true })
+keymap.set({ "t", "i" }, "<A-k>", "<C-\\><C-N><C-w>k", { noremap = true })
+keymap.set({ "t", "i" }, "<A-l>", "<C-\\><C-N><C-w>l", { noremap = true })
+keymap.set("n", "<A-h>", "<C-w>h", { noremap = true })
+keymap.set("n", "<A-j>", "<C-w>j", { noremap = true })
+keymap.set("n", "<A-k>", "<C-w>k", { noremap = true })
+keymap.set("n", "<A-l>", "<C-w>l", { noremap = true })
---
+++ lua/config/options.lua
@@ -0,0 +1,11 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+-- LazyVim.terminal.setup("nu")
+-- INFO: settings to set nushell as the shell for the :! command
+-- --
+-- path to the Nushell executable
+vim.opt.sh = "nu"
+vim.opt.shellcmdflag = "-c"
+vim.opt.shellquote = ""
+vim.opt.shellxquote = ""
